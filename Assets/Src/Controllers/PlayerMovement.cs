96509f340deffb509cd11efd6a7cfe8b147bf913
Luis Mendoza <luisjomen2a@gmail.cim>
2020-08-16 19:15:50 +0200

    feat(PlayerMovement): add new script for movement handling.
    - Add new camera for player tracking that switches in while playing.

---
+++ Assets/Src/Controllers/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Player m_player;
+
+    private World m_world;
+
+    public Camera m_camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // there should be only one Player in the game.
+        m_player = gameObject.GetComponent<Player>();
+
+        // there should be only one World in the game.
+        m_world = gameObject.GetComponent<World>();
+
+        // there should be only one World in the game.
+        m_camera = gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Keypad2))
+        {
+            m_player.Place(m_player.position.x, m_player.position.y - 1);
+        }
+        if (Input.GetKeyUp(KeyCode.Keypad8))
+        {
+            m_player.Place(m_player.position.x, m_player.position.y + 1);
+        }
+        if (Input.GetKeyUp(KeyCode.Keypad4))
+        {
+            m_player.Place(m_player.position.x - 1, m_player.position.y);
+        }
+        if (Input.GetKeyUp(KeyCode.Keypad6))
+        {
+            m_player.Place(m_player.position.x + 1, m_player.position.y);
+        }
+        m_camera.transform.position = new Vector3(m_player.position.x, 10, m_player.position.y);
+        m_camera.transform.LookAt(new Vector3(m_player.position.x, 2, m_player.position.y));
+    }
+}
--- Assets/Src/Managers/GameManager.cs
+++ Assets/Src/Managers/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Camera m_camera;
+    private Camera m_camera;
 
     public Player m_player;
 
@@ -17,6 +17,9 @@ public class GameManager : MonoBehaviour
     {
         m_world = gameObject.AddComponent(typeof(World)) as World;
         m_player = gameObject.AddComponent(typeof(Player)) as Player;
+        m_camera = gameObject.AddComponent(typeof(Camera)) as Camera;
+        m_camera.enabled = false;
+
 
         m_playerLight.range = 200;
         m_playerLight.intensity = 2;
@@ -35,36 +38,8 @@ public class GameManager : MonoBehaviour
             m_player.Place(rndPos.x, rndPos.y);
             m_camera.transform.position = new Vector3(rndPos.x, 10, rndPos.y - 2);
             m_camera.transform.LookAt(new Vector3(rndPos.x, 2, rndPos.y));
+            m_camera.enabled = true;
             m_playerLight.transform.position = new Vector3(rndPos.x, 2, rndPos.y);
-        }
-        // Player movement.
-        if (Input.GetKeyUp(KeyCode.Keypad2))
-        {
-            m_player.Place(m_player.position.x, m_player.position.y - 1);
-            m_camera.transform.position = new Vector3(m_player.position.x, 10, m_player.position.y - 2);
-            m_camera.transform.LookAt(new Vector3(m_player.position.x, 2, m_player.position.y));
-            m_playerLight.transform.position = new Vector3(m_player.position.x, 3, m_player.position.y);
-        }
-        if (Input.GetKeyUp(KeyCode.Keypad8))
-        {
-            m_player.Place(m_player.position.x, m_player.position.y + 1);
-            m_camera.transform.position = new Vector3(m_player.position.x, 10, m_player.position.y - 2);
-            m_camera.transform.LookAt(new Vector3(m_player.position.x, 2, m_player.position.y));
-            m_playerLight.transform.position = new Vector3(m_player.position.x, 3, m_player.position.y);
-        }
-        if (Input.GetKeyUp(KeyCode.Keypad4))
-        {
-            m_player.Place(m_player.position.x - 1, m_player.position.y);
-            m_camera.transform.position = new Vector3(m_player.position.x, 10, m_player.position.y - 2);
-            m_camera.transform.LookAt(new Vector3(m_player.position.x, 2, m_player.position.y));
-            m_playerLight.transform.position = new Vector3(m_player.position.x, 3, m_player.position.y);
-        }
-        if (Input.GetKeyUp(KeyCode.Keypad6))
-        {
-            m_player.Place(m_player.position.x + 1, m_player.position.y);
-            m_camera.transform.position = new Vector3(m_player.position.x, 10, m_player.position.y - 2);
-            m_camera.transform.LookAt(new Vector3(m_player.position.x, 2, m_player.position.y));
-            m_playerLight.transform.position = new Vector3(m_player.position.x, 3, m_player.position.y);
         }
     }
 }
